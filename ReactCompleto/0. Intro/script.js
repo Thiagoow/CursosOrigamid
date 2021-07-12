350b867b67b9a9f749cdd5b0a505e7865068d0e4
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-07-12 14:32:46 -0300

    .

--- ReactCompleto/0. Intro/script.js
+++ ReactCompleto/0. Intro/script.js
@@ -1,5 +1,32 @@
-const menu = {
-  state: "active"
-};
+/* Função normal: 
+function upperCase(text) {
+  //Não esquecer do retorno, se não dá "undefined"
+  return text.toUpperCase();
+}*/
+/* Como arrow function: */
+const upperCase = (text) => text.toUpperCase();
 
-console.log(menu.state.toUpperCase);
+console.log(upperCase("texto"));
+
+// --------------Desestruturando:
+/* Original: 
+function mousePosition(event) {
+  //Defino 1 variável pra cada item que eu quero do objeto event:
+  const posY = event.clientY;
+  const posX = event.clientX;
+  console.log(posX, posY);
+}*/
+
+/* Desestruturada: 
+function mousePosition(event) {
+  //Pego só o que eu quero dentro do objeto event:
+  const { clientX, clientY } = event;
+  console.log(clientX, clientY);
+}*/
+
+/*  MAIS desestruturada: */
+function mousePosition(clientX, clientY) {
+  //Define como argumento aquilo que eu quero e exibo no console:
+  console.log(clientX, clientY);
+}
+document.addEventListener("click", mousePosition);
