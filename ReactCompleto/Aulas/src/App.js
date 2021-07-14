e18ba18e59a1c618004e7aca9249170e7b7b6181
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-07-14 18:48:53 -0300

    0212 Começando

--- ReactCompleto/Aulas/src/App.js
+++ ReactCompleto/Aulas/src/App.js
@@ -1,28 +1,7 @@
 import React from "react";
 
-/* Exemplos básicos de uso: */
-const Title = (props) => {
-  return <h2>{props.txt}</h2>;
-};
-const Texto = ({ cor, fundo, txt }) => {
-  return <p style={{ color: cor, background: fundo }}>{txt}</p>;
-};
-
-/* Um componente também pode possuir child's. */
-
 const App = () => {
-  return (
-    <>
-      <div align="center">
-        <Title txt="App" />
-        <Texto cor="white" fundo="red" txt="Teste :D" />
-      </div>
-
-      <div align="center" style={{ marginTop: "2rem" }}>
-        <Title txt="ChildrenProps:" />
-      </div>
-    </>
-  );
+  return <div align="center">App</div>;
 };
 
 export default App;
--- ReactCompleto/Aulas/src/Aulas/01. Início/Props.js
+++ ReactCompleto/Aulas/src/Aulas/01. Início/Props.js
@@ -2,13 +2,16 @@ import React from "react";
 
 /* Exemplos básicos de uso: */
 const Title = (props) => {
-  return <h1>{props.txt}</h1>;
+  return <h2>{props.txt}</h2>;
 };
 const Texto = ({ cor, fundo, txt }) => {
   return <p style={{ color: cor, background: fundo }}>{txt}</p>;
 };
 
-/* Children: */
+/* Um componente também pode possuir child's. Pra isso, é só
+abrir e fechar o componente no return, inserindo elementos 
+filhos dentro dele. Tendo esses elementos filhos, as mesmas props
+ativas do componente pai. */
 
 const App = () => {
   return (
@@ -18,7 +21,7 @@ const App = () => {
         <Texto cor="white" fundo="red" txt="Teste :D" />
       </div>
 
-      <div align="center">
+      <div align="center" style={{ marginTop: "2rem" }}>
         <Title txt="ChildrenProps:" />
       </div>
     </>
