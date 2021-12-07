e390bcee914d686351b4cf1a0e6616c09e1a9fec
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-12-07 19:16:12 -0300

    Traversing e Manipulação :D

--- Programação Front End/04. JavaScriptCompleto/ManipulaçõesDOM/main.js
+++ Programação Front End/04. JavaScriptCompleto/ManipulaçõesDOM/main.js
@@ -1,5 +1,5 @@
 //1-------------- Selecionando elementos no DOM:
-console.warn("Exemplo1:");
+console.error("Exemplo1 - querySelector:");
 
 /* 
 Seletores mais usados e estáticos
@@ -34,8 +34,8 @@ console.log(h2Animais);
 const paragrafos = document.querySelectorAll("p");
 console.log(paragrafos[--paragrafos.length]);
 
-//2-------------- forEach e Arrow Function:
-console.warn("Exemplo2:");
+//2----------------------forEach e Arrow Function:
+console.error("Exemplo2 - forEach:");
 const imgs = document.querySelectorAll("img");
 
 let i = 0;
@@ -59,8 +59,8 @@ const titulosArray = Array.from(titulos);
 //Arrow function em linha única (sem {}):
 titulosArray.forEach((item) => console.log(item));
 
-//3-------------- Classes e Atributos:
-console.warn("Exemplo3:");
+//3------------------------Classes e Atributos:
+console.error("Exemplo3 - classList & attributes:");
 
 const menu = document.querySelector(".menu");
 
@@ -114,13 +114,23 @@ imgs.forEach((img) => {
   console.log(img, possuiAtributo);
 });
 
-// Modifica o href do link externo no menu:
-const link = document.querySelector('a[href^="http"]'); //Seleciona a tag A que começa com "http"
-link.setAttribute("href", "https://www.origamid.com/");
-console.log(link);
+//Seleciona todas as tags A que começam com "http"
+const externalLinks = document.querySelectorAll('a[href^="http"]');
+// Modifica o href dos links externos:
+externalLinks.forEach((one) => {
+  one.setAttribute("href", "https://www.origamid.com/");
+});
+console.log(externalLinks);
 
-//4-------------- Dimensões e Distâncias:
-console.warn("Exemplo4:");
+//Previne o comportamento padrão deles:
+externalLinks.forEach((one) => {
+  one.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
+});
+
+//4-------------------Dimensões e Distâncias:
+console.error("Exemplo4 - width & height:");
 
 const section = document.querySelector(".animais-lista");
 console.log(section);
@@ -173,8 +183,8 @@ if (small.matches) {
   console.log("Tela > que 800px");
 }
 
-//----------Exercícios:
-console.warn("Exercícios:");
+//----------------Exercícios 1:
+console.error("Exercícios 1:");
 
 /* Verifica a distância da 1º imagem
 em relação ao topo da página: */
@@ -219,5 +229,145 @@ if (browserSmall) {
   menu.classList.add("menu-mobile");
 }
 
-//5-------------- Eventos:
-console.warn("Exemplo5:");
+//5-------------------------- Eventos:
+console.error("Exemplo5 - Eventos:");
+
+/*A boa prática é sempre separar a função
+que será executada no evento, do próprio evento,
+assim: */
+function callbackFunction(event) {
+  console.log("Clicou na:");
+  console.log(img);
+  //Argumento "event" ou "e" mais explicado abaixo:
+  console.log(event);
+}
+//Executa a função de callback acima quando clicar na img:
+img.addEventListener("click", callbackFunction);
+
+/* Mas também, pode ser encontrado assim:
+
+img.addEventListener("click", () => {
+  console.log("Clicou na:");
+  console.log(img);
+});
+*/
+
+// Propriedades do "event" ou "e":
+function callbackEvent(event) {
+  console.log("Propriedades do event:");
+  const currentTarget = event.currentTarget; // this
+  const target = event.target; // onde o clique ocorreu
+  const type = event.type; // tipo de evento
+  const path = event.path; //caminho do evento
+  console.warn(".currentTarget:");
+  console.log(currentTarget);
+  console.warn(".target:");
+  console.log(target);
+  console.warn(".type:");
+  console.log(type);
+  console.warn(".path:");
+  console.log(path);
+  console.warn("this:");
+  console.log(this); //Geralmente igual ao event.currentTarget
+}
+h2Animais.addEventListener("click", callbackEvent);
+
+/*----------Tipos diferentes de eventos:
+const h1 = document.querySelector("h1");
+function callback(event) {
+  console.log(event.type, event);
+}
+h1.addEventListener("click", callback);
+h1.addEventListener("mouseenter", callback);
+window.addEventListener("scroll", callback);
+window.addEventListener("resize", callback);
+window.addEventListener("keydown", callback);
+*/
+
+function handleKeyboard(event) {
+  switch (event.key) {
+    case "a":
+      console.log("Você digitou o A");
+      break;
+    case "b":
+      console.log("Você digitou o B");
+      break;
+    case "f":
+      document.body.classList.toggle("fullscreen");
+      console.log("Fullscreen toggle :D");
+      break;
+    default:
+      console.log("Digite A, B ou T :)");
+      break;
+  }
+}
+window.addEventListener("keydown", handleKeyboard);
+
+//Também podemos add um evento para cada item (forEach + addEventListener):
+function handleImgs(event) {
+  //Pra cada img mostra o src dela:
+  const src = event.currentTarget.getAttribute("src");
+  console.log(src);
+}
+
+imgs.forEach((img) => {
+  img.addEventListener("click", handleImgs);
+});
+
+//----------------Exercícios 2:
+console.error("Exercícios 2:");
+
+/* Quando o usuário clicar nos links internos do site,
+adicione a classe ativo ao item clicado e remova dos
+demais itens caso eles possuam a mesma. Previna
+o comportamento padrão desses links */
+
+//const linksInterno = document.querySelectorAll('[href^="#"]');
+console.log(linksInterno);
+function handleInternalLinks(event) {
+  event.preventDefault();
+  //Remove a classe ativo de todos os itens:
+  linksInterno.forEach((link) => {
+    link.classList.remove("ativo");
+  });
+  //E só deixa a do clicado atualmente:
+  event.currentTarget.classList.add("ativo");
+}
+
+linksInterno.forEach((link) => {
+  link.addEventListener("click", handleInternalLinks);
+});
+
+/* Selecione todos os elementos do site começando a partir do body,
+ao clique mostre exatamente quais elementos estão sendo clicados */
+const allBodyElements = document.querySelectorAll("body *"); //<- Todos que tão na tag body
+
+allBodyElements.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    /* Mostra onde clicou: 
+    console.log("Tu clicou no:");
+    console.log(event.currentTarget);*/
+    /* Remove o elemento clicado: 
+    event.currentTarget.remove();*/
+  });
+});
+
+// Se o usuário clicar na tecla "t", aumente todo o texto do site.
+function handleClickT(event) {
+  console.log(event.key);
+  let tKey,
+    plusKey = false;
+
+  if (event.key === "t") {
+    tKey, (plusKey = true);
+    //document.documentElement.classList.toggle("textoMaior");
+    console.log("Texto aumentado :)");
+  }
+}
+
+window.addEventListener("keydown", handleClickT);
+
+//6----------------Traversing e Manipulação:
+console.error("Exemplo6 - Traversing e Manipulação:");
+
+//
